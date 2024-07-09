fb1757c4e807e7cde328faa4d6ca141592ce6218
Mihnea2003 <bro_bigger@yahoo.ro>
2024-07-09 19:26:41 +0300

    Making the models
    i made Songs,Albums and Artists models.

---
+++ music-library-backend/models/Artists.js
@@ -0,0 +1,26 @@
+const Album = require('./Album');
+
+class Artist {
+    constructor(name, albums) {
+        this.name = name;
+        this.albums = albums.map(album => new Album(album.title, album.songs, album.description));
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getAlbums() {
+        return this.albums;
+    }
+
+    setAlbums(albums) {
+        this.albums = albums.map(album => new Album(album.title, album.songs, album.description));
+    }
+}
+
+module.exports = Artist;
